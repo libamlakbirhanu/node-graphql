d7e8b1680be6fc2add95ed4393bf1ecd53c92be5
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-06-02 14:29:23 +0300

    added dotenv package to access environment variables

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
+
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const app = express();
 const PORT = 5000;
@@ -23,7 +26,10 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.mongo_uri).then(() => {
-  console.log("database connected successfully");
-  app.listen(PORT, () => console.log("server started successfully"));
-});
+mongoose
+  .connect(process.env.mongo_uri)
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(PORT, () => console.log("server started successfully"));
+  })
+  .catch((err) => console.log(err));
